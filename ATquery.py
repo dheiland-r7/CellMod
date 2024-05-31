14bb48ef00a90ccf29c265ec1eca9c13acad99ff
Percent X <deral_heiland@rapid7.com>
2024-05-31 16:13:43 -0400

    Update ATquery.py
    Added some comments

--- ATquery.py
+++ ATquery.py
@@ -6,13 +6,13 @@ import sys
 if __name__ == '__main__':
 
 # setup serial output parameters
-    ser = serial.Serial('/dev/ttyUSB2', 115200, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB2', 115200, timeout=1) # Change to point to FTDI connected to Cell Module
     ser.reset_input_buffer()
     time_now  = time.strftime("%Y%m%d%H%M%S")
 
 # Read from file
     # filepath = './AT_UBLOX_TEST.txt'
-    filepath = './AT_UBLOX_READ.txt'
+    filepath = './AT_UBLOX_READ.txt' # Enter the correct file name and path for AT commands to test with
     with open(filepath) as fp:
        line = fp.readline()
        cnt = 1
